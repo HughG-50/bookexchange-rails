a40c6a5610939e20fb2f68c9a5b0214fc72edead
HughG-50 <hugh.greethead@gmail.com>
2020-05-03 14:14:05 +1000

    Seed db setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,70 @@
-# Old seeding using Faker Gem
+#Initial Seeds file
 
-# initial_total_genres = 20
-# initial_total_authors = 50
+list_of_fiction_genres = [
+    "Action", 
+    "Adventure",
+    "Children's", 
+    "Comic", 
+    "Crime",
+    "Drama",
+    "Fairytale",
+    "Fantasy",
+    "Horror",
+    "Mystery",
+    "Poetry",
+    "Romance",
+    "Satire",
+    "Science Fiction",
+    "Thriller",
+    "Young adult"
+]
+    
+list_of_non_fiction_genres = [ 
+    "Art", 
+    "Autobiography", 
+    "Biography", 
+    "Cookbook", 
+    "Diary", 
+    "Dictionary",
+    "Encyclopedia",
+    "Guide",
+    "Health",
+    "History",
+    "Journal",
+    "History",
+    "Memoir",
+    "Textbook",
+    "Science",
+    "Self help",
+    "Travel",
+    "True crime"
+]
 
-# for i in 1..initial_total_genres
-#     name = Faker::Book.genre
-#     Genre.create(name: name)
-#     puts "#{name} created"
-# end
+# seed Fiction Genres
+for genre in list_of_fiction_genres
+    Genre.create(name: genre, is_fiction: true)
+    puts "#{genre} Genre created"
+end
 
-# for i in 1..initial_total_authors
-#     name = Faker::Book.author
-#     Author.create(name: Faker::Book.author)
-#     puts "#{name} created"
-# end
+# seed Non-Fiction Genres
+for genre in list_of_non_fiction_genres
+    Genre.create(name: genre, is_fiction: false)
+    puts "#{genre} - Genre created"
+end
+
+
+# Author seeding - just dummy values that aren't needed
+initial_total_authors = 50
+
+for i in 1..initial_total_authors
+    name = Faker::Book.author
+    Author.create(name: Faker::Book.author)
+    puts "#{name} - Author created"
+end
+
+	
+
+	
+
+	
 
